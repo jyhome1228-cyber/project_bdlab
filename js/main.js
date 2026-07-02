2edc839ba668c810bfd3c2349fc8a0a5c6d41460
Nineworks Framework <jyhome1228@gmail.com>
2026-07-03 08:42:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,11 @@
-const BDLAB_IMAGES = {
-  logo: "./assets/images/common/bdlab-logo.png",
+// BDLab Main Script
 
-  homeHero: "./assets/images/home/hero-01.jpg",
-  homeAbout: "./assets/images/home/about-01.jpg",
-  homeProcess: "./assets/images/home/process-01.jpg",
+const menuButton = document.querySelector(".bd-menu-button");
+const headerNav = document.querySelector(".bd-header-nav");
 
-  archiveDefault: "./assets/images/archive/terracle-01.jpg",
-
-  terracle: "./assets/images/archive/terracle-01.jpg",
-  masitta: "./assets/images/archive/masitta-01.jpg",
-  kekomi: "./assets/images/archive/kekomi-01.jpg",
-  odebell: "./assets/images/archive/odebell-01.jpg",
-
-  journal01: "./assets/images/journal/journal-01.jpg",
-  journal02: "./assets/images/journal/journal-02.jpg"
-};
-
-// 기본 이미지 삽입
-document.querySelectorAll("[data-img-key]").forEach((image) => {
-  const key = image.dataset.imgKey;
-  const src = BDLAB_IMAGES[key];
-
-  if (src) {
-    image.src = src;
-  }
-});
-
-// Archive hover preview
-const archivePreview = document.getElementById("archivePreviewImage");
-const projectItems = document.querySelectorAll("[data-preview-key]");
-
-projectItems.forEach((item) => {
-  item.addEventListener("mouseenter", () => {
-    const key = item.dataset.previewKey;
-    const src = BDLAB_IMAGES[key];
-
-    if (archivePreview && src) {
-      archivePreview.src = src;
-    }
+if (menuButton && headerNav) {
+  menuButton.addEventListener("click", () => {
+    headerNav.classList.toggle("is-open");
+    menuButton.classList.toggle("is-active");
   });
-});
+}
