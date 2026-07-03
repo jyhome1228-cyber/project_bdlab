7590f239be84bf1b08a51b1a7b722243ce15b6ed
Nineworks Framework <jyhome1228@gmail.com>
2026-07-03 10:16:21 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// BDLab Main Script
-
 const menuButton = document.querySelector(".bd-menu-button");
 const headerNav = document.querySelector(".bd-header-nav");
 
@@ -7,5 +5,14 @@ if (menuButton && headerNav) {
   menuButton.addEventListener("click", () => {
     headerNav.classList.toggle("is-open");
     menuButton.classList.toggle("is-active");
+  });
+
+  const navLinks = headerNav.querySelectorAll("a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      headerNav.classList.remove("is-open");
+      menuButton.classList.remove("is-active");
+    });
   });
 }
